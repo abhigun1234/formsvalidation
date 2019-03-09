fd458f4e9335acc12c38db3a33691aff5262f5aa
Gungun <gungun@Gunguns-MacBook-Pro.local>
2019-03-09 18:20:27 +0530

    first commit

--- src/app/tdf/tdf.component.ts
+++ src/app/tdf/tdf.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tdf',
   templateUrl: './tdf.component.html',
-  styleUrls: ['./tdf.component.css']
+  styles: ['input.ng-invalid { border-left:5px solid red } input.ng-valid{border-left:5px solid green}' ]
 })
 export class TdfComponent implements OnInit {
 
